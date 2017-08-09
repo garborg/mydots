c2159971d56d8992125c6ad606ae66add489860f
Sean Garborg <se@n-garb.org>
2017-08-08 20:25:48 -0500

    Fix scripted vim plugin installation

--- allsetup.bash
+++ allsetup.bash
@@ -3,10 +3,7 @@
 . linkdots.bash
 
 # Get vim up and running
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-# TODO: why are these commands not running? (thought they were before)
-vim -es -c ':PlugInstall' -c ':GoInstallBinaries' -c ':qa'
-
+# Install vim plugins specified in .vimrc
+vim -e -c ':PlugInstall' -c ':GoInstallBinaries' -c ':qa'
