6c514c8d54d9c2f7ae5b46d64ac278871911cdc9
Sean Garborg <se@n-garb.org>
2019-09-04 14:01:21 -0500

    Conda devtool install updates

--- basicsetup.sh
+++ basicsetup.sh
@@ -7,7 +7,8 @@ set -e
 # Install fzf. It's already sourced by .bashrc
 "$HOME/.fzf/install" --all --no-update-rc
 
-# TODO: Install ripgrep
+# TODO: install fzf via conda
+./installconda.sh
 
 # Get vim up and running
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
