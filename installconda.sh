663613082e5c47ad466a026a75ef7636b1ece54c
Sean Garborg <sean.garborg@chrobinson.com>
2020-02-19 08:31:02 -0600

    Fix conda install script

--- installconda.sh
+++ installconda.sh
@@ -5,11 +5,13 @@ set -e
 CONDA_DIR=${HOME}/.miniconda
 
 # Python
-if [ ! -d ${CONDA_DIR} ]; then
+if [ ! -d "${CONDA_DIR}" ]; then
   if [ ! -f ./Miniconda3-latest-Linux-x86_64.sh ]; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
   fi
-  bash Miniconda3-latest-Linux-x86_64.sh -b -p ${CONDA_DIR}
+  bash Miniconda3-latest-Linux-x86_64.sh -b -p "${CONDA_DIR}"
 fi
+
+. "${HOME}/.bashrc"
 
 conda env update --quiet -f ./environment.yml -n base
