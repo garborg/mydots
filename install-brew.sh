1f24d9c59686277592cc09f4ee662a91ff6e929a
Sean Garborg <se@n-garb.org>
2021-06-27 13:37:45 -0500

    Expand on brew installs

--- install-brew.sh
+++ install-brew.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
 if ! command -v brew > /dev/null 2>&1; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     # - Add Homebrew to your PATH in $HOME/.zprofile:
     echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
     eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
-brew install fd fzf git ripgrep tmux
+# utilities
+brew install direnv fd fzf git gnupg ripgrep tmux
 
-# fzf
+# add fzf integrations
 # --xdg: dotfile location respects $XDG_CONFIG
 # --key-bindings, --completion: activate them
 # --update-rc: no-op while lines in .rc files are current, keep on in case of changes
 $(brew --prefix fzf)/install --xdg --key-bindings --completion --update-rc
+
+# asdf (homebrew install is problematic: https://github.com/asdf-vm/asdf/issues/785)
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+cd ~/.asdf && git checkout "$(git describe --abbrev=0 --tags)"
+asdf plugin-add nodejs
+asdf plugin-add julia
+
+# apps:
+# chrome, firefox, notion, etc.
+# Code:
+# - from: https://code.visualstudio.com/download
+# - extensions: Julia, Vim
+
