e69a259b96d9bd3431b48ea118b18e868d004275
Sean Garborg <se@n-garb.org>
2021-06-27 21:44:12 -0500

    Cleanup

--- install-brew.sh
+++ install-brew.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if ! command -v brew > /dev/null 2>&1; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-    # - Add Homebrew to your PATH in $HOME/.zprofile:
-    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
-    eval "$(/opt/homebrew/bin/brew shellenv)"
+  # - Add Homebrew to your PATH in $HOME/.zprofile:
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 # utilities
--- install-source.sh
+++ install-source.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # asdf (homebrew install is problematic: https://github.com/asdf-vm/asdf/issues/785)
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-cd ~/.asdf && git checkout "$(git describe --abbrev=0 --tags)"
-asdf plugin-add nodejs
-asdf plugin-add julia
+if [ ! -d ~/.asdf ]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+  cd ~/.asdf && git checkout "$(git describe --abbrev=0 --tags)"
+  asdf plugin-add nodejs
+  asdf plugin-add julia
+fi
